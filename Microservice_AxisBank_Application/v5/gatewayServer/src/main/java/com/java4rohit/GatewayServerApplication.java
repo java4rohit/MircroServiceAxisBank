c69092ad97683b35a276e6cfd21fd5232cde57a8
Rohit Yadav <60982731+java4rohit@users.noreply.github.com>
2025-06-10 00:24:57 +0530

    Basic Gateway server is setup

--- Microservice_AxisBank_Application/v5/gatewayServer/src/main/java/com/java4rohit/GatewayServerApplication.java
+++ Microservice_AxisBank_Application/v5/gatewayServer/src/main/java/com/java4rohit/GatewayServerApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GatewayServerApplication {
-
 	public static void main(String[] args) {
+
 		SpringApplication.run(GatewayServerApplication.class, args);
 	}
 
